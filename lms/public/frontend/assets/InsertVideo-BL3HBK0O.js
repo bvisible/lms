7a342d76a8111aaa3f03fa7bd5829d63fdae9f8e
Jérémy Christillin <jeremy@neoservice.ai>
2025-12-22 12:13:50 +0100

    feat(build): Pre-build frontend assets to avoid server-side builds

---
+++ lms/public/frontend/assets/InsertVideo-BL3HBK0O.js
@@ -0,0 +1,2 @@
+import{d as s,bH as t,bI as a,bJ as d}from"./index-BW7I4ACl.js";const p=s({__name:"InsertVideo",props:{editor:{}},setup(e){const o=e;function n(){o.editor.chain().focus().selectAndUploadVideo().run()}return(r,i)=>t(r.$slots,"default",a(d({onClick:n})))}});export{p as default};
+//# sourceMappingURL=InsertVideo-BL3HBK0O.js.map
